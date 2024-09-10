05cab948939a4a992137e13449979d17a248acb5
leejm521 <leejm521@gmail.com>
2024-09-10 17:48:28 +0900

    최종

--- js/group.js
+++ js/group.js
@@ -32,4 +32,14 @@ $(document).ready(function(){
             $(".quick").show();
         }
     });
+
+    //------------------------마우스-----------------------//
+    document.addEventListener('mousemove', (e) => {
+        let mouseX = e.pageX + 10; // document의 x좌표값
+        let mouseY = e.pageY + 10; // document의 y좌표값
+    
+        let cursor = document.querySelector('.cursor');
+        cursor.style.left = mouseX + 'px';
+        cursor.style.top = mouseY + 'px';
+    });
     });
